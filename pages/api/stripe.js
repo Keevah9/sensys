bbb14e902b573e36374ef6b87eb77df82c938e13
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-08-24 22:19:09 +0100

    sensys

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+import { loadStripe } from "@stripe/stripe-js";
+const stripe = new ('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
           }),
 
           success_url: `${req.headers.origin}/success`,
-          cancel_url: `${req.headers.origin}/canceled`,
+          cancel_url: `${req.headers.origin}/?canceled=true`,
         };
 
       // Create Checkout Sessions from body params.
