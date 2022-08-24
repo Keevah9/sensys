d917d65ed0254dc1188eea7645b80e45dbce233e
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-08-24 21:51:36 +0100

    sensys

--- components/Single.jsx
+++ components/Single.jsx
@@ -4,10 +4,10 @@ const Single = () => {
   return (
     <div className='pt-16 pb-12 sm:pb-0 sm:flex justify-between mx-auto  px-16 w-full xl:w-5/6 xl:mx-auto relative' id='features'>
       <div className='sm:w-1/2'>
-        <Jump>
+        {/* <Jump> */}
           <h2 className='text-6xl  bold uppercase text-center sm:text-left text-green-900 '>
             Bold, Strong & Beautiful</h2>
-        </Jump>
+        {/* </Jump> */}
         <p className='py-4 sm:text-left text-center '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, autem.</p>
       </div>
     </div>
